7dd489da5d6fe21b6cdfdf77d621fb61d701af76
Nicholai Goodall <kranoscorp@gmail.com>
2014-09-21 03:57:11 +0000

    more pseudocode

--- server.js
+++ server.js
@@ -6,26 +6,26 @@ var BFS = function(G, v) { // procedure BFS(G,v) is
   var result = [];
   while(Q.length !== 0) { // while Q is not empty loop
     var t = Q.shift(); // t ← Q.dequeue()
-    console.log("col " + t);
+    //console.log("col " + t);
     result.push(t);
     if(t === 2) { // if t is what we are looking for then
       return result; // return t
     } // end if
     for(var e = 0; e < G[t].length; e++) { // for all edges e in G.adjacentEdges(t) loop
       var u = G[t][e]; // u ← G.adjacentVertex(t,e)
-      console.log("row " + e + ", val " + u);
+      //console.log("row " + e + ", val " + u);
       if(G[t][e] === 1 && !(e in V)) { // if u is not in V then
-        console.log("The tip is in");
+        //console.log("The tip is in");
         V[e] = true; // add u to V
         Q.push(e); // enqueue u onto Q
       } // end if
     } // end loop
   } // end loop
-  return "fuck you"; // return none
+  return 0; // return none
 }; // end BFS
-var checker = BFS([[0,1,0,1],[1,0,0,0],[0,0,0,1],[1,0,1,0]],1);
-console.log(checker.toString());
-console.log("buttplugs");
+//var checker = BFS([[0,1,0,1],[1,0,0,0],[0,0,0,1],[1,0,1,0]],1);
+//console.log(checker.toString());
+//console.log("check2");
 var DFSR = function(G, v, V) { //procedure DFS(G,v):
   if(V === undefined) {
     V = {};
@@ -39,5 +39,11 @@ var DFSR = function(G, v, V) { //procedure DFS(G,v):
     }
   }
 };
-checker = DFSR([[0,1,0,1],[1,0,0,0],[0,0,0,1],[0,0,1,1]],1);
-console.log(checker.toString());
+//checker = DFSR([[0,1,0,1],[1,0,0,0],[0,0,0,1],[0,0,1,1]],1);
+//console.log(checker.toString());
+
+//load json graph
+//make output
+//add bfs to output
+//add dfs to output
+//output
